1be2aea6ec49588c862ceb5210c5c6cd131a9022
danz_souza <danielsouza@gmail.com>
2026-03-27 18:16:22 -0300

    corrigindo o código do pixel

--- Script/script.js
+++ Script/script.js
@@ -1,12 +1,11 @@
 const botaoWpp = document.getElementById("btn-wpp");
 const botaoTg = document.getElementById("btn-tg");
 
-function trackLead(event, url, nome) {
-  event.preventDefault();
+function trackGrupoVIP(event, url, nomeDoCanal) {
+  event.preventDefault(); 
 
-  fbq("track", "Lead", {
-    content_name: nome,
-    content_category: "Grupo VIP",
+  fbq("trackCustom", "GrupoVIP", {
+    canal: nomeDoCanal,
     pixel_id: "885360972067096",
   });
 
@@ -15,13 +14,14 @@ function trackLead(event, url, nome) {
   }, 500);
 }
 
+
 botaoWpp.addEventListener("click", function (e) {
   const linkwpp = "https://chat.whatsapp.com/CZpfQP8SbmdIFx8cJSlo2g?mode=gi_t";
-  trackLead(e, linkwpp, "Grupo VIP - WhatsApp");
+  trackGrupoVIP(e, linkwpp, "whatsapp");
 });
 
 
 botaoTg.addEventListener("click", function (e) {
   const linktg = "https://t.me/lojaestylomaria";
-  trackLead(e, linktg, "Grupo VIP - Telegram");
+  trackGrupoVIP(e, linktg, "telegram");
 });
